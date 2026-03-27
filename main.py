051c4333db5c3a4e2ebe6a0f8b7781956bc147b8
echandia27 <jp.echandia@gmail.com>
2026-03-27 13:12:05 -0500

    termminado trabajo

--- main.py
+++ main.py
@@ -32,6 +32,8 @@ print(df.duplicated().sum())
 df = df.drop_duplicates()
 df.isnull().sum()
 
+#aqui se muentras el top 10 de los paises 
+print("\n top paises\n")
 sales_by_country = df.groupby('country')['sales']. sum().sort_values(ascending=False)
 print(sales_by_country.head(10))
 
@@ -39,3 +41,31 @@ sales_by_country.head(10).plot(kind='bar')
 plt.title("top paises por ingresos")
 plt.ylabel("ventas")
 plt.show()
+
+#ahora mostraremos producto mas vendido
+print("\n top producto \n")
+sales_by_product = df.groupby('productline')['sales'].sum().sort_values(ascending=False)
+print(sales_by_product)
+
+sales_by_product.plot(kind='bar')
+plt.title("ventas por linea de producto")
+plt.ylabel("ventas")
+plt.show()
+
+#crecimiento atraves de los años 
+print("\n crecimiento en años \n")
+sales_by_year = df.groupby('year_id')['sales'].sum()
+print(sales_by_year)
+
+sales_by_year.plot(marker='o')
+plt.title("ventas por año")
+plt.ylabel("ventas")
+plt.show()
+
+print("\n numero de meses por año \n")
+print(df.groupby('year_id')['month_id'].nunique())
+
+#donde debe invertir la empresa
+print("\n donde invertir \n")
+sales_country_product = df.groupby(['country', 'productline'])['sales'].sum().sort_values(ascending=False)
+print(sales_country_product.head(10))
